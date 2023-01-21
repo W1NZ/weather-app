bed13a9eccad95d729d30d3513e3df533a43229a
Mariusz <m4riusz.cz@gmail.com>
2023-01-21 23:11:14 +0100

    Weather App 1.0

---
+++ app.js
@@ -0,0 +1,75 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const errorMessage = document.querySelector(".error");
+const date = document.querySelector(".date");
+const cityName = document.querySelector(".cityName");
+const img = document.querySelector("img");
+const temperatureDescription = document.querySelector(
+  ".temperatureDescription"
+);
+const temperature = document.querySelector(".temperature");
+const feelsTemperature = document.querySelector(".feelsTemperature");
+const pressure = document.querySelector(".pressure");
+const humidity = document.querySelector(".humidity");
+const windSpeed = document.querySelector(".windSpeed");
+
+const apiLink = "https://api.openweathermap.org/data/2.5/weather?q=";
+const apiKey = "&appid=e1b56b6b65e2550b6543eb1b4a5ee99b";
+const apiUnit = "&units=metric";
+const apiLang = "&lang=pl";
+
+const checkWeather = () => {
+  const apiCity = input.value;
+  const URL = apiLink + apiCity + apiKey + apiUnit + apiLang;
+  console.log(URL);
+
+  axios
+    .get(URL)
+    .then((response) => {
+      console.log(response.data);
+
+      date.textContent = `${new Date().toString().slice(4, 21)}`;
+      cityName.textContent = `${response.data.name}, ${response.data.sys.country}`;
+      img.src = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`;
+      temperatureDescription.textContent = `${response.data.weather[0].description}`;
+      temperature.textContent = `${Math.floor(response.data.main.temp)} °C`;
+      feelsTemperature.textContent = `${Math.floor(
+        response.data.main.feels_like
+      )} °C`;
+      pressure.textContent = `${response.data.main.pressure} hPa`;
+      humidity.textContent = `${response.data.main.humidity} %`;
+      windSpeed.textContent = `${Math.floor(response.data.wind.speed)} m/s`;
+      errorMessage.textContent = "";
+    })
+    .catch((error) => {
+      if (error.response.data.cod === "404") {
+        errorMessage.textContent = `Nie znaleziono miasta o nazwie ${input.value}`;
+      }
+      [
+        cityName,
+        temperature,
+        feelsTemperature,
+        temperatureDescription,
+        pressure,
+        humidity,
+        windSpeed,
+        date,
+      ].forEach((el) => (el.textContent = ""));
+      img.setAttribute("src", "");
+    })
+    .finally(() => {
+      input.value = "";
+    });
+};
+window.addEventListener("DOMContentLoaded", function () {
+  input.defaultValue = "Warsaw";
+  checkWeather();
+});
+
+button.addEventListener("click", checkWeather);
+
+input.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    checkWeather();
+  }
+});
